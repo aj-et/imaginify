c2ad4faedbc43442f66fe395fff0e071003be321
aj-et <aaron.tumbs@gmail.com>
2024-02-16 12:32:36 -0700

    Started Imaginify Project

---
+++ README.md
@@ -0,0 +1,17 @@
+Libraries used
+* ShadCN - for UI
+ npx shadcn-ui@latest init
+
+
+Things Learned
+
+Next JS
+    Route Groups
+    Layouts
+    Dynamic Routes
+
+ShadCN components installed
+npx shadcn-ui@latest add button
+npx shadcn-ui@latest add sheet
+
+mongoDB and mongoose
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+// Each transformation will have its own id
+// It will look like this:
+// localhost:3000/transformations/[id] - dynamic routes
